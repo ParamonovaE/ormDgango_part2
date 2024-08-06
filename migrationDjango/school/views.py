faf73b3d7a11131a1c9e0e2a95f9ffd2b20b6eec
ParamonovaE <128289272+ParamonovaE@users.noreply.github.com>
2024-08-06 22:04:48 +0300

    Update views.py

--- migrationDjango/school/views.py
+++ migrationDjango/school/views.py
@@ -5,7 +5,7 @@ from .models import Student
 
 
 def students_list(request):
-    object_list = Student.objects.all().order_by('group')
+    object_list = Student.objects.all().order_by('group').prefetch_related('teachers')
     template = 'school/students_list.html'
     context = {
         'object_list': object_list,
